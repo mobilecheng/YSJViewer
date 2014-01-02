ce8de60c1ddc42603e85639cae09e29f93dbd77d
Kevin Zhang <mobilecheng@qq.com>
2014-01-03 00:47:05 +0800

    Third

--- YSJViewer/AppDelegate.m
+++ YSJViewer/AppDelegate.m
@@ -13,7 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    NSLog(@"Test github. Will update.");
+    NSLog(@"Test github. Will update. haha...");
     
     return YES;
 }
