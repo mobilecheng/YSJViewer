95284894634c29e51c65d1e3f497bd7a0bb6949e
TMC_MAC_02 <TMC_MAC_02@TMC-MAC-02tekiiMac.local>
2014-01-03 10:22:26 +0800

    test push on TMC

--- YSJViewer/ViewController.m
+++ YSJViewer/ViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    NSLog(@"Test");
+    NSLog(@"Test 2014");
 }
 
 - (void)didReceiveMemoryWarning
