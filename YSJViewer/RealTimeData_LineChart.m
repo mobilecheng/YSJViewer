f83583dd8c6352af1c3c9eb3ed02178fae92c2a6
Kevin Zhang <mobilecheng@KevindeMacBook-Pro-Retina.local>
2014-03-09 00:31:14 +0800

    line chart test

--- YSJViewer/RealTimeData_LineChart.m
+++ YSJViewer/RealTimeData_LineChart.m
@@ -9,6 +9,7 @@
 #import "RealTimeData_LineChart.h"
 #import "GlobalValue.h"
 #import "LCLineChartView.h"
+#import "ViewLineChart.h"
 
 #define degreesToRadians(x) (M_PI * x / 180.0)
 #define SECS_PER_DAY (86400)
@@ -46,10 +47,14 @@
                    customHeaderFields:nil];
     
     //
-    [self api_GetRecentItemData];
+//    [self api_GetRecentItemData];
     
     // test
     [self showLineChart];
+    
+//    ViewLineChart *customView = [[ViewLineChart alloc] initWithFrame:CGRectMake(0, 0,
+//                            568, self.view.frame.size.height)];
+//    [self.view addSubview:customView];
 }
 
 
@@ -145,123 +150,52 @@
 - (void) showLineChart
 {
     
-    {
-        self.formatter = [[NSDateFormatter alloc] init];
-        [self.formatter setDateFormat:[NSDateFormatter dateFormatFromTemplate:@"yyyyMMMd" options:0 locale:[NSLocale currentLocale]]];
+    
+    LCLineChartData *d1 = [LCLineChartData new];
+//    LCLineChartData *d1 = d1x;
+    
+    NSDate *date1 = [[NSDate date] dateByAddingTimeInterval:((-3) * SECS_PER_DAY)];
+    NSDate *date2 = [[NSDate date] dateByAddingTimeInterval:((2) * SECS_PER_DAY)];
+    
+    d1.xMin = [date1 timeIntervalSinceReferenceDate];
+    d1.xMax = [date2 timeIntervalSinceReferenceDate];
+    
+//    d1.title = @"Foobarbang";
+    d1.color = [UIColor redColor];
+    d1.itemCount = 6;
+    
+    NSMutableArray *arr = [NSMutableArray array];
+    for(NSUInteger i = 0; i < 4; ++i) {
+        [arr addObject:@(d1.xMin + (rand() / (float)RAND_MAX) * (d1.xMax - d1.xMin))];
+    }
+    [arr addObject:@(d1.xMin)];
+    [arr addObject:@(d1.xMax)];
+    [arr sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+        return [obj1 compare:obj2];
+    }];
+    
+    NSMutableArray *arr2 = [NSMutableArray array];
+    for(NSUInteger i = 0; i < 6; ++i) {
+        [arr2 addObject:@((rand() / (float)RAND_MAX) * 6)];
     }
     
-    LCLineChartData *d1x = [LCLineChartData new];
-    {
-        LCLineChartData *d1 = d1x;
-        // el-cheapo next/prev day. Don't use this in your Real Code (use NSDateComponents or objc-utils instead)
-        NSDate *date1 = [[NSDate date] dateByAddingTimeInterval:((-3) * SECS_PER_DAY)];
-        NSDate *date2 = [[NSDate date] dateByAddingTimeInterval:((2) * SECS_PER_DAY)];
-        d1.xMin = [date1 timeIntervalSinceReferenceDate];
-        d1.xMax = [date2 timeIntervalSinceReferenceDate];
-        d1.title = @"Foobarbang";
-        d1.color = [UIColor redColor];
-        d1.itemCount = 6;
-        NSMutableArray *arr = [NSMutableArray array];
-        for(NSUInteger i = 0; i < 4; ++i) {
-            [arr addObject:@(d1.xMin + (rand() / (float)RAND_MAX) * (d1.xMax - d1.xMin))];
-        }
-        [arr addObject:@(d1.xMin)];
-        [arr addObject:@(d1.xMax)];
-        [arr sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
-            return [obj1 compare:obj2];
-        }];
-        NSMutableArray *arr2 = [NSMutableArray array];
-        for(NSUInteger i = 0; i < 6; ++i) {
-            [arr2 addObject:@((rand() / (float)RAND_MAX) * 6)];
-        }
-        d1.getData = ^(NSUInteger item) {
-            float x = [arr[item] floatValue];
-            float y = [arr2[item] floatValue];
-            NSString *label1 = [self.formatter stringFromDate:[date1 dateByAddingTimeInterval:x]];
-            NSString *label2 = [NSString stringWithFormat:@"%f", y];
-            return [LCLineChartDataItem dataItemWithX:x y:y xLabel:label1 dataLabel:label2];
-        };
-    }
+    d1.getData = ^(NSUInteger item) {
+        float x = [arr[item] floatValue];
+        float y = [arr2[item] floatValue];
+        NSString *label1 = [self.formatter stringFromDate:[date1 dateByAddingTimeInterval:x]];
+        NSString *label2 = [NSString stringWithFormat:@"%f", y];
+        return [LCLineChartDataItem dataItemWithX:x y:y xLabel:label1 dataLabel:label2];
+    };
     
-    LCLineChartData *d2x = [LCLineChartData new];
-    {
-        LCLineChartData *d1 = d2x;
-        NSDate *date1 = [[NSDate date] dateByAddingTimeInterval:((-3) * SECS_PER_DAY)];
-        NSDate *date2 = [[NSDate date] dateByAddingTimeInterval:((2) * SECS_PER_DAY)];
-        d1.xMin = [date1 timeIntervalSinceReferenceDate];
-        d1.xMax = [date2 timeIntervalSinceReferenceDate];
-        d1.title = @"Bar";
-        d1.color = [UIColor blueColor];
-        d1.itemCount = 8;
-        NSMutableArray *arr = [NSMutableArray array];
-        for(NSUInteger i = 0; i < d1.itemCount - 2; ++i) {
-            [arr addObject:@(d1.xMin + (rand() / (float)RAND_MAX) * (d1.xMax - d1.xMin))];
-        }
-        [arr addObject:@(d1.xMin)];
-        [arr addObject:@(d1.xMax)];
-        [arr sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
-            return [obj1 compare:obj2];
-        }];
-        NSMutableArray *arr2 = [NSMutableArray array];
-        for(NSUInteger i = 0; i < d1.itemCount; ++i) {
-            [arr2 addObject:@((rand() / (float)RAND_MAX) * 6)];
-        }
-        d1.getData = ^(NSUInteger item) {
-            float x = [arr[item] floatValue];
-            float y = [arr2[item] floatValue];
-            NSString *label1 = [self.formatter stringFromDate:[date1 dateByAddingTimeInterval:x]];
-            NSString *label2 = [NSString stringWithFormat:@"%f", y];
-            return [LCLineChartDataItem dataItemWithX:x y:y xLabel:label1 dataLabel:label2];
-        };
-    }
-    
-    LCLineChartView *chartView = [[LCLineChartView alloc] initWithFrame:CGRectMake(20, 400, 500, 300)];
+    // Add to view.
+    LCLineChartView *chartView = [[LCLineChartView alloc] initWithFrame:CGRectMake(0, 80, 500, 200)];
     chartView.yMin = 0;
     chartView.yMax = 6;
-    chartView.ySteps = @[@"1.0",@"2.0",@"3.0",@"4.0",@"5.0",@"A big label at 6.0"];
-    chartView.data = @[d1x,d2x];
-    
-    //    chartView.drawsDataPoints = NO; // Uncomment to turn off circles at data points.
-    //    chartView.drawsDataLines = NO; // Uncomment to turn off lines connecting data points.
-    //    chartView.backgroundColor = [UIColor colorWithWhite:0.95 alpha:1.0]; // Uncomment for custom background color.
+    chartView.ySteps = @[@"1.0",@"2.0",@"3.0",@"4.0",@"5.0",@"6.0"];
+    chartView.data = @[d1];
     
     [self.view addSubview:chartView];
     
-    {
-        LCLineChartData *d = [LCLineChartData new];
-        d.xMin = 1;
-        d.xMax = 31;
-        d.title = @"The title for the legend";
-        d.color = [UIColor redColor];
-        d.itemCount = 10;
-        
-        NSMutableArray *vals = [NSMutableArray new];
-        for(NSUInteger i = 0; i < d.itemCount; ++i)
-            [vals addObject:@((rand() / (float)RAND_MAX) * (31 - 1) + 1)];
-        [vals sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
-            return [obj1 compare:obj2];
-        }];
-        d.getData = ^(NSUInteger item) {
-            float x = [vals[item] floatValue];
-            float y = powf(2, x / 7);
-            NSString *label1 = [NSString stringWithFormat:@"%d", item];
-            NSString *label2 = [NSString stringWithFormat:@"%f", y];
-            return [LCLineChartDataItem dataItemWithX:x y:y xLabel:label1 dataLabel:label2];
-        };
-        
-        LCLineChartView *chartView = [[LCLineChartView alloc] initWithFrame:CGRectMake(20, 700, 500, 300)];
-        chartView.yMin = 0;
-        chartView.yMax = powf(2, 31 / 7) + 0.5;
-        chartView.ySteps = @[@"0.0",
-                             [NSString stringWithFormat:@"%.02f", chartView.yMax / 2],
-                             [NSString stringWithFormat:@"%.02f", chartView.yMax]];
-        chartView.xStepsCount = 5;
-        chartView.data = @[d];
-        
-        chartView.axisLabelColor = [UIColor blueColor];
-        
-        [self.view addSubview:chartView];
-    }
 }
 
 
---
+++ YSJViewer/ViewLineChart.h
@@ -0,0 +1,13 @@
+//
+//  ViewLineChart.h
+//  YSJViewer
+//
+//  Created by Kevin Zhang on 14-3-8.
+//  Copyright (c) 2014年 Reload Digital Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewLineChart : UIView
+
+@end
